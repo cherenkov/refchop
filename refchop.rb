ad059dde66d900b1973a0d3465b707e4180976b5
cherenkov <chrnkv@gmail.com>
2010-12-20 12:32:40 +0900

    + set content_type

--- refchop.rb
+++ refchop.rb
@@ -22,11 +22,10 @@ get '/*' do
   img_host = img_url.host
   img_path = img_url.path
   mime_type = MIME::Types.type_for(img_path)[0].to_s
-
   begin
     Net::HTTP.start(img_host, 80) {|http|
+      content_type mime_type
       res = http.get(img_path)
-      #datauri = 'data:' + mime_type + ';base64,' + Base64.encode64(res.body).gsub(/\n/,"")
       response.body = res.body
     }
   rescue => e
